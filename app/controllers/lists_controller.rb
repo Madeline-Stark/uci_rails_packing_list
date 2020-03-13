6e5f31dd7907b587b6d222344370d7520910c0cf
github username <github email address>
2020-03-12 18:02:26 -0700

    Office hours

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,50 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: [:edit, :update, :show, :delete]
+
+
+  def new
+    @list = List.new
+    @list.items.build
+  end
+
+  def create
+    binding.pry
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else 
+      render :new 
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else 
+      render :edit 
+    end
+  end
+
+  def delete
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :items_attributes => [:name])
+  end 
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
+end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
     has_many :items
     validates :name, presence: true, uniqueness: true
+    accepts_nested_attributes_for :items
 end
